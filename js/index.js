ae8c2593766b8c9aac915477955eb1a3502b9087
tbadaczewski <badaczewski@gmail.com>
2014-10-02 20:32:57 -0400

    cc

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
-document.addEventListener("deviceready", function() {
-    navigator.geolocation.getCurrentPosition(function(position) {
-    	alert("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude); 
-    });
+document.addEventListener("load", function() {
+	var options = {
+		enableHighAccuracy: true,
+		timeout: 5000,
+		maximumAge: 0
+	};
+
+	function success(position) {
+		alert("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude); 
+	}
+
+	function error(err) {
+    	alert('ERROR(' + err.code + '): ' + err.message);    	
+    }
+
+	navigator.geolocation.getCurrentPosition(success, error, options);
 }, false);
