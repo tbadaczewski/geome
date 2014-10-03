cd209426b060d660f3473c561ec326c705fc5ad6
tbadaczewski <badaczewski@gmail.com>
2014-10-02 22:27:45 -0400

    df

--- js/index.js
+++ js/index.js
@@ -37,10 +37,10 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        //navigator.notification.alert('Received Event: ' + id);
+        alert('Received Event: ' + id);
 
         navigator.geolocation.getCurrentPosition(function(data) {
-            navigator.notification.alert("Latitude: " + data.coords.latitude + " - Longitude: " + data.coords.longitude);
+            alert("Latitude: " + data.coords.latitude + " - Longitude: " + data.coords.longitude);
         });
     }
 };
