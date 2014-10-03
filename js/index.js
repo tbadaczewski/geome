c1d992a24e2c136c8b3ecbf244d3fa9068e7a4fb
tbadaczewski <badaczewski@gmail.com>
2014-10-02 20:14:17 -0400

    cvv

--- js/index.js
+++ js/index.js
@@ -1,23 +1,15 @@
 document.addEventListener("deviceready", function() {
-	//var location = document.getElementById("location");
-	alert("Loaded");
-	//getLocation();
+	getLocation();
 }, false);
 
-/*
 function getLocation() {
-	var location = document.getElementById("location");
-
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-        location.innerHTML = "Geolocation is not supported by this browser.";
+        alert("Geolocation is not supported by this browser.");
     }
 }
 
 function showPosition(position) {
-	var location = document.getElementById("location");
-
-    location.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude; 
+    alert("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude); 
 }
-*/
