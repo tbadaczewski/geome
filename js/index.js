a77486e185e1d5c08946177bd8bb2b8a3d12cd06
tbadaczewski <badaczewski@gmail.com>
2014-10-02 20:20:20 -0400

    rr

--- js/index.js
+++ js/index.js
@@ -1,19 +1,5 @@
 document.addEventListener("deviceready", function() {
-    if (navigator.geolocation) {
-        alert("Geolocation is supported by this browser.");
-    } else {
-        alert("Geolocation is not supported by this browser.");
-    }
+    navigator.geolocation.getCurrentPosition(function(position) {
+    	alert("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude); 
+    });
 }, false);
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        alert("Geolocation is not supported by this browser.");
-    }
-}
-
-function showPosition(position) {
-    alert("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude); 
-}
