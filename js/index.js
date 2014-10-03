870946a71df35f0055517e97d2e2924c4a212058
tbadaczewski <badaczewski@gmail.com>
2014-10-02 20:27:43 -0400

    asd

--- js/index.js
+++ js/index.js
@@ -1,10 +1,5 @@
 document.addEventListener("deviceready", function() {
-	    navigator.geolocation.getCurrentPosition(
-	    	function(position) {
-	    		alert("Success");
-	    		//alert("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude); 
-	    	},
-	    	function(error){
-				alert("Error"); 
-	    	});
+    navigator.geolocation.getCurrentPosition(function(position) {
+    	alert("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude); 
+    });
 }, false);
